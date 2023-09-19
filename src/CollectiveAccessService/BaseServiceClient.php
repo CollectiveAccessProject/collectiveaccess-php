2a1961a13099926b867e9ce3563f32e878674447
CollectiveAccess <seth@collectiveaccess.org>
2023-09-19 12:37:42 -0400

    Update URL for 2.0 services

--- src/CollectiveAccessService/BaseServiceClient.php
+++ src/CollectiveAccessService/BaseServiceClient.php
@@ -23,7 +23,7 @@ abstract class BaseServiceClient {
 	# ----------------------------------------------
 	public function __construct($ps_base_url, $ps_service) {
 		$this->ops_service_url = $ps_base_url."/service.php/".$ps_service;
-		$this->ops_auth_url = $ps_base_url."/service.php/auth/login";
+		$this->ops_auth_url = $ps_base_url."/service.php/json/auth/login";
 
 		// try to get user and password/key from environment
 		if(defined('__CA_SERVICE_API_USER__') && defined('__CA_SERVICE_API_KEY__')) {
